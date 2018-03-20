2883814dfac84cd0284166d9b49e135441c72dd0
Accelerator <mr.hamidi.okba@gmail.com>
2018-03-20 20:29:08 +0100

    creating new folder with existing project to test html to pdf functionality

---
+++ CV_to_pdf_GeneratorTest/js/app.js
@@ -0,0 +1,22 @@
+var CVApp = angular.module('CVApp', ['ngMaterial']);
+
+CVApp.controller('CVController', function CVController($scope, $http)
+{
+  $http(
+  {
+    method: 'GET',
+    url: './data/okba-cv1-EN.json',
+    headers : {'Accept' : 'application/json'}
+  }).then(function successCallback(response)
+  {
+    console.error("success response : ", response);
+    $scope.currentUser = response.data;
+    // this callback will be called asynchronously
+    // when the response is available
+  }, function errorCallback(response)
+  {
+    console.error("error response : ", response);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+});
