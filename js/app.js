9ed1bfcaad9ae81b71aa2a672b912f5b2f27f058
Accelerator <mr.hamidi.okba@gmail.com>
2018-03-25 20:22:56 +0200

    removed jsPdf test

--- js/app.js
+++ js/app.js
@@ -1,22 +1,39 @@
 var CVApp = angular.module('CVApp', ['ngMaterial']);
 
-CVApp.controller('CVController', function CVController($scope, $http)
-{
-  $http(
-  {
-    method: 'GET',
-    url: './data/okba-cv1-EN.json',
-    headers : {'Accept' : 'application/json'}
-  }).then(function successCallback(response)
-  {
-    console.error("success response : ", response);
-    $scope.currentUser = response.data;
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response)
-  {
-    console.error("error response : ", response);
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+CVApp.controller('CVController', function ($scope, $http) {
+
+  var self = this;
+
+  this.initData = function() {
+    getJson();
+  }
+
+  function getJson() {
+    if (false) {
+      console.error("get parameter from query string");
+    }else {
+      getDefaultCv();
+    }
+  }
+
+  function getDefaultCv() {
+    $http({
+      method: 'GET',
+      url: './data/okba-cv1-EN.json',
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(function successCallback(response) {
+      console.error("success response : ", response);
+      $scope.currentUser = response.data;
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      console.error("error response : ", response);
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
+  }
+
+  this.initData();
 });
