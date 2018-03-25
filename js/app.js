c626def143eb38659d83e87a4dd616d2c335362b
Accelerator <mr.hamidi.okba@gmail.com>
2018-03-25 21:55:55 +0200

    test on jsPDF

--- js/app.js
+++ js/app.js
@@ -28,7 +28,9 @@ CVApp.controller('CVController', function($scope, $http) {
       $scope.currentUser = response.data;
       // this callback will be called asynchronously
       // when the response is available
-      CreatePdf();
+      setTimeout(function() {
+        CreatePdfFromHTML(document.getElementsByTagName('cv-content')[0]);
+      }, 1500);
     }, function errorCallback(response) {
       console.error("error response : ", response);
       // called asynchronously if an error occurs
@@ -36,12 +38,26 @@ CVApp.controller('CVController', function($scope, $http) {
     });
   }
 
-  function CreatePdf() {
-    // Default export is a4 paper, portrait, using milimeters for units
-    var doc = new jsPDF()
+  function CreatePdfFromHTML(htmlContainer) {
+    var HtmlElementDoundary = htmlContainer.getBoundingClientRect();
+    console.error("htmlContainer: ", htmlContainer);
+    console.error("HtmlElementDoundary: ", HtmlElementDoundary);
 
-    doc.text('Hello world!', 10, 10)
-    doc.save('a4.pdf')
+    var pdf = new jsPDF('p', 'pt', 'letter');
+     var canvas = pdf.canvas;
+     //canvas.width = HtmlElementDoundary //= 8.5 * 72;
+     html2canvas(document.body, {
+         canvas:canvas,
+         onrendered: function(canvas) {
+             var iframe = document.createElement('iframe');
+             iframe.setAttribute('style','position:absolute;right:0; top:0; bottom:0; height:100%; width:500px');
+             document.body.appendChild(iframe);
+             iframe.src = pdf.output('datauristring');
+            //var div = document.createElement('pre');
+            //div.innerText=pdf.output();
+            //document.body.appendChild(div);
+         }
+     });
   }
 
   this.initData();
