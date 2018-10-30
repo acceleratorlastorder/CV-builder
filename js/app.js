ae666a62e6670ed2b409dc667ffd889577667a64
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2018-10-30 23:20:19 +0100

    add querystring reader, to manage the lang of the content with ?lang=  can be FR or EN

--- js/app.js
+++ js/app.js
@@ -1,19 +1,59 @@
-var CVApp = angular.module('CVApp', ['ngMaterial']);
+var CVApp = angular.module("CVApp", ["ngMaterial"]);
 
-CVApp.controller('CVController', function ($scope, $http) {
+CVApp.controller("CVController", function($scope, $http) {
 
   var self = this;
 
+  this.defaultLang = "EN";
+  this.currentLang = "";
+  this.searchParam = {};
+
   this.initData = function() {
-    getJson();
+    this.searchParam = getSearchQueryParam();
+    this.getLang();
+    this.getJson();
   }
 
-  function getJson() {
-    if (false) {
-      console.error("get parameter from query string");
-    }else {
+  this.getLang = function() {
+    this.currentLang = this.searchParam["lang"];
+    if (!this.currentLang) {
+      this.currentLang = this.defaultLang;
+    }
+  }
+
+  this.getJson = function() {
+    if (this.currentLang) {
+      var url = "./data/okba-cv1-" + this.currentLang + ".json";
+      $http({
+        method: "GET",
+        url: url,
+        headers: {
+          "Accept": "application/json"
+        }
+      }).then(function successCallback(response) {
+        console.error("success response : ", response);
+        $scope.currentUser = response.data;
+      }, function errorCallback(response) {
+        console.error("error response : ", response);
+        getDefaultCv();
+      });
+    } else {
+      console.log("get parameter from query string gathering default version !");
       getDefaultCv();
     }
+  }
+
+
+  function getSearchQueryParam() {
+    var queryString = window.location.search;
+    var arrayOfParam = queryString.slice(1, queryString.length).split("&");
+    var result = {};
+    var tempArray = null;
+    for (var i = 0; i < arrayOfParam.length; i++) {
+      tempArray = arrayOfParam[i].split("=");
+      result[tempArray[0]] = tempArray[1];
+    }
+    return result;
   }
 
   function getDefaultCv() {
